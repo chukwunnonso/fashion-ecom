6a955fb20ff52d2ec0a1b9628e42f8d2bf392a00
muqsitadam <muqsitadam@gmail.com>
2022-12-06 13:17:35 +0100

    Working on the landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import HeaderHero from '../components/LandingPage/HeaderHero'
+import ImageSection from '../components/LandingPage/ImageSection'
 
 const Home = () => {
   return (
     <div className='container'>
-        <Navbar/> 
-        <Footer/>
-        
+        <Navbar/>
+        <HeaderHero/>
+        <ImageSection/>
+        <Footer/>        
     </div>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    './src/components/LandingPage/HeaderHero.jsx',
+    './src/components/LandingPage/ImageSection.jsx',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
