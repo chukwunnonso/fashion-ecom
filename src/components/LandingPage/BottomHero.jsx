0ac54fef43f7d5128e660c5f6a83c96f023f7766
Chukwunnonso <cnnonso@gmail.com>
2022-12-09 10:06:22 -0500

    worked-on-productlist-page

--- src/components/LandingPage/BottomHero.jsx
+++ src/components/LandingPage/BottomHero.jsx
@@ -1,3 +1,77 @@
+// import React from "react";
+// import image from "../../assets/hero-bottom.png"
+
+// const BottomHero = () => {
+//     return(
+//         <div className="relative mt-20">
+//             <img className="image" src={image} alt="big-image" />
+//             <div className="">
+//                 <p className="text-black">OWN THE DAY</p>
+//                 <p className="text-black">TACHEN 19</p>
+//                 <button className="button bg-black">SHOP COLLECTION</button>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default BottomHero
+
+ import React from "react";
+ import image from "../../assets/hero-bottom.png"
+
+
+
+
+
+
+const BottomHero = () => {
+         return(
+            <div className="relative">
+                <img src={image} alt="dummy-image" className="w-full" />
+                <button class="absolute top-0 bg-blue-500 text-white p-2 rounded hover:bg-blue-800 m-2">Button</button>
+            </div>
+
+         )
+     }
+export default BottomHero
+
+
+
+
+
+
+
+
+
+// import React from "react";
+// import image from "../../assets/hero-bottom.png"
+
+// const BottomHero = () => {
+//     return(
+//         <div className="bg-gradient-to-tl from-purple-900 to-green-700 h-96 w-full relative">
+//             <img src={image} className="w-full h-full object-cover absolute mix-blend-overlay" alt="big-image" />
+//             <div className="p-24">
+//                 <h1 className="text-white text-6xl font-bold">OWN THE DAY</h1>
+//                 <h2 className="text-white text-3xl font-light">TACHEN 19</h2>
+//                 <button className="button bg-black text-white">SHOP COLLECTION</button>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default BottomHero
+
+
+
+
+
+
+
+
+/**
+ 
+
+
 import React from "react";
 import image from "../../assets/hero-bottom.png"
 
@@ -6,12 +80,13 @@ const BottomHero = () => {
         <div className="relative mt-20">
             <img className="image" src={image} alt="big-image" />
             <div className="">
-                <p className="">OWN THE DAY</p>
-                <p className="">TACHEN 19</p>
-                <button className="button">SHOP COLLECTION</button>
+                <p className="text-black">OWN THE DAY</p>
+                <p className="text-black">TACHEN 19</p>
+                <button className="button bg-black">SHOP COLLECTION</button>
             </div>
         </div>
     )
 }
 
 export default BottomHero
+ */
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,11 +1,75 @@
-import React from "react";
+// import React from "react";
+
+// const ProductList = () => {
+//     return(
+//         <div>
+//             ProductList
+//         </div>
+//     )
+// }
+
+// export default ProductList
+
+
+
+///////Let me write styling below
+
+
+
+import React, {useState, useEffect} from 'react'
+import '../App.css';
+//import { json } from 'react-router-dom';
 
 const ProductList = () => {
-    return(
-        <div>
-            ProductList
+
+    const [fake, setFake] =useState([])
+    console.log(fake)
+    useEffect(() => {
+        
+        fakeStore();
+    }, [])
+
+    const fakeStore = async()=> {
+        const response = await fetch("https://fakestoreapi.com/products");
+        //console.log(response)
+        const jsonData =await response.json()
+        //console.log(jsonData);
+        setFake(jsonData);
+    }
+    //fakeStore();
+    //https://fakestoreapi.com/products
+  return (
+    <>
+    <div className='product-container'>
+        {fake.map((values)=> {
+            return(
+                <>
+                <div className='box'>
+                    <div className="content">
+                    <h5 className='product-h5'>{values.title}</h5>
+                    <p className='product-p'>{values.description}</p>
+                    </div>
+                    <img className='product-image' src={values.image}/>
+                      <h5 className='product-h5'>{values.price}</h5>
+                </div>    
+                
+                </>
+            )
+        })}
+        {/* <div className='box'>
+        <div className="content">
+            <h5>Title</h5>
+            <p>Description</p>
         </div>
-    )
+        <h1>Image</h1>
+        </div> */}
+        
+        
+        </div>
+        </>
+  )
 }
 
 export default ProductList
+
+
