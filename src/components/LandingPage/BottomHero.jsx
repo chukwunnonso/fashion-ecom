311ecedefbc2994b7231113f3f01b3ae1a64b17d
muqsitadam <muqsitadam@gmail.com>
2022-12-14 10:56:43 +0100

    Correction to Bottom hero

--- src/components/LandingPage/BottomHero.jsx
+++ src/components/LandingPage/BottomHero.jsx
@@ -5,7 +5,7 @@ const BottomHero = () => {
     return(
         // Not yet responsive
         <div className="lg:relative mt-20 w-full">
-            <img className="image" src={image} alt="big-image" />
+            <img className="image w-full" src={image} alt="big-image" />
             <div className="lg:absolute  top-30 lg:left-40 lg:right-20 bottom-20">
                 <p className="text-black text-sm font-normal leading-6">OWN THE DAY</p>
                 <p className="text-black text-2xl lg:text-6xl font-bold">TACHEN 19</p>
